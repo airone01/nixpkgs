4ddaaff0ceca784fa0a26ebe8c315e23f8f231ef
aktaboot <akhtaboot@protonmail.com>
2025-06-07 21:31:07 +0200

    mullvad-vpn: build from source, inherit mullvad version, wip

--- pkgs/by-name/mu/mullvad-vpn/package.nix
+++ pkgs/by-name/mu/mullvad-vpn/package.nix
@@ -1,188 +1,104 @@
 {
-  stdenv,
   lib,
-  fetchurl,
-  dpkg,
-  alsa-lib,
-  atk,
-  cairo,
-  cups,
-  dbus,
-  expat,
-  fontconfig,
-  freetype,
-  gdk-pixbuf,
-  glib,
-  pango,
-  nspr,
-  nss,
-  gtk3,
-  libgbm,
-  libGL,
-  wayland,
-  libxtst,
-  libxscrnsaver,
-  libxrender,
-  libxrandr,
-  libxi,
-  libxfixes,
-  libxext,
-  libxdamage,
-  libxcursor,
-  libxcomposite,
-  libx11,
-  libxshmfence,
-  libxcb,
-  autoPatchelfHook,
-  systemd,
-  libnotify,
-  libappindicator,
+  buildNpmPackage,
+  replaceVars,
+  grpc-tools,
   makeWrapper,
-  coreutils,
-  gnugrep,
-  iproute2,
-
-  versionCheckHook,
+  electron,
+  mullvad,
+  nodejs_22,
 }:
-
-let
-  deps = [
-    alsa-lib
-    atk
-    cairo
-    cups
-    dbus
-    expat
-    fontconfig
-    freetype
-    gdk-pixbuf
-    glib
-    pango
-    gtk3
-    libappindicator
-    libnotify
-    libgbm
-    libx11
-    libxscrnsaver
-    libxcomposite
-    libxcursor
-    libxdamage
-    libxext
-    libxfixes
-    libxi
-    libxrandr
-    libxrender
-    libxtst
-    libxcb
-    libxshmfence
-    nspr
-    nss
-    systemd
-  ];
-
-  selectSystem =
-    attrs:
-    attrs.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
-
-  platform = selectSystem {
-    x86_64-linux = "amd64";
-    aarch64-linux = "arm64";
-  };
-
-  hash = selectSystem {
-    x86_64-linux = "sha256-ewJ/XxqwVLF3/MsiN+AZ+jQodMr+JmPtpbcdXe6HNPo=";
-    aarch64-linux = "sha256-hpuLpDA3PMrlOkF172f0PZY+cGe2gBkRTWCwwwYJwQo=";
-  };
-in
-
-stdenv.mkDerivation (finalAttrs: {
+buildNpmPackage rec {
   pname = "mullvad-vpn";
-  version = "2026.2";
+  inherit (mullvad) src version;
+  nodejs = nodejs_22;
 
-  __structuredAttrs = true;
-  strictDeps = true;
-
-  src = fetchurl {
-    url = "https://github.com/mullvad/mullvadvpn-app/releases/download/${finalAttrs.version}/MullvadVPN-${finalAttrs.version}_${platform}.deb";
-    inherit hash;
-  };
-
-  nativeBuildInputs = [
-    autoPatchelfHook
-    dpkg
-    makeWrapper
-  ];
-
-  buildInputs = deps;
-
-  dontBuild = true;
-  dontConfigure = true;
-
-  runtimeDependencies = [
-    (lib.getLib systemd)
-    iproute2
-    libGL
-    libnotify
-    libappindicator
-    wayland
+  patches = [
+    (replaceVars ./distribution.cjs.patch {
+      inherit version;
+      electron-dist = electron.dist;
+    })
+    # ./management-interface-build.sh.patch
   ];
 
   postPatch = ''
-    patchShebangs opt/Mullvad\ VPN/mullvad-vpn
+    cd desktop/
+  '';
+
+  npmDepsHash = "sha256-hPH4xm6wZ7gGUMs6GiOEv1TgURU2Tmfq5bnWwDxcYrs=";
+
+  npmFlags = [ "--ignore-scripts" ];
+
+  nativeBuildInputs = [
+    makeWrapper
+    grpc-tools
+  ];
+
+  makeCacheWritable = true;
+
+  buildInputs = [
+    electron
+  ];
+
+  env = {
+    ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+    ELECTRON_OVERRIDE_DIST_PATH = electron.dist; # TODO: figure out why this is needed
+  };
+
+  postConfigure = ''
+    ln -s ${lib.getBin mullvad}/bin/* ../dist-assets/
+    # ln -s ${lib.getExe' grpc-tools "protoc"} node_modules/grpc-tools/bin/protoc
+    # ln -s ${lib.getExe' grpc-tools "grpc_node_plugin"} node_modules/grpc-tools/bin/grpc_node_plugin
+  '';
+
+  # FIXME Hack: For some reason the build works fine on the 2nd attempt
+  buildPhase = ''
+    npm -w windows-utils run build-typescript
+    npm -w management-interface run build
+    npm -w mullvad-vpn run pack:linux
   '';
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/share/mullvad $out/bin
+    mkdir -p $out/share/mullvad-vpn/
+    cp -r ../dist/*-unpacked/{locales,resources{,.pak}} $out/share/mullvad-vpn/
+    cp ../graphics/icon{-square.svg,.svg} $out/share/mullvad-vpn/resources/
 
-    mv usr/share/* $out/share
-    mv usr/bin/* $out/bin
-    mv opt/Mullvad\ VPN/* $out/share/mullvad
 
-    ln -s $out/share/mullvad/mullvad-{gui,vpn} $out/bin/
-    ln -sf $out/share/mullvad/resources/mullvad-problem-report $out/bin/mullvad-problem-report
-
-    wrapProgram $out/bin/mullvad-vpn \
-      --set MULLVAD_DISABLE_UPDATE_NOTIFICATION 1 \
-      --prefix PATH : ${
-        lib.makeBinPath [
-          coreutils
-          gnugrep
-        ]
-      }
-
-    wrapProgram $out/bin/mullvad-daemon \
-        --set-default MULLVAD_RESOURCE_DIR "$out/share/mullvad/resources"
-
-    wrapProgram $out/bin/mullvad-gui \
-      --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime=true}}"
-
-    sed -i "s|Exec.*$|Exec=$out/bin/mullvad-vpn $U|" $out/share/applications/mullvad-vpn.desktop
+    makeWrapper ${lib.getExe electron} $out/bin/mullvad-vpn \
+        --add-flags $out/share/mullvad-vpn/resources/app.asar \
+        --set MULLVAD_DISABLE_UPDATE_NOTIFICATION 1 \
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+        --inherit-argv0
 
     runHook postInstall
   '';
 
-  nativeInstallCheckInputs = [
-    versionCheckHook
-  ];
-  doInstallCheck = true;
+  postInstall = ''
+    mkdir -p $out/share/applications
 
-  passthru.updateScript = ./update.sh;
+    cat > $out/share/applications/${pname}.desktop <<EOF
+    [Desktop Entry]
+    Type=Application
+    Icon=$out/share/mullvad-vpn/resources/icon.svg
+    Exec=$out/bin/mullvad-vpn
+    Name=Mullvad VPN
+    GenericName=VPN Client
+    Categories=Application;Other;
+    EOF
+  '';
 
   meta = {
     homepage = "https://github.com/mullvad/mullvadvpn-app";
     description = "Client for Mullvad VPN";
-    changelog = "https://github.com/mullvad/mullvadvpn-app/blob/${finalAttrs.version}/CHANGELOG.md";
-    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    changelog = "https://github.com/mullvad/mullvadvpn-app/blob/${version}/CHANGELOG.md";
     license = lib.licenses.gpl3Only;
-    mainProgram = "mullvad-vpn";
-    platforms = lib.platforms.unix;
-    badPlatforms = [ lib.systems.inspect.patterns.isDarwin ];
+    inherit (electron.meta) platforms;
     maintainers = with lib.maintainers; [
-      jackr
-      airone01
-      sigmasquadron
+      Br1ght0ne
+      ymarkus
+      ataraxiasjel
     ];
   };
-})
+}
